3dc0a9be1188cfe1bd8446a7f1d66b438d50efb1
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-12-08 15:19:26 +0100

    added CORS header

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,10 @@ const wikiUrl = 'https://www.wikidata.org';
 
 const apiUrl = `${wikiUrl}/w/api.php?action=query&list=allrevisions&arvdir=newer&arvlimit=50&arvstart=2020-10-01T00:00:00Z&arvend=2020-11-01T00:00:00Z`;
 
-fetch( apiUrl )
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch(apiUrl, {
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+})
+    .then(response => response.json())
+    .then(data => console.log(data));
